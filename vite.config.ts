e8a7a7cd4bb8dd8b9fc4809613c0acd8f8310acc
hrong1 <hrong1@ucsc.edu>
2025-11-08 17:48:41 -0800

    base set up for project

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr';
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  base: '/Space-tourism-multi-page-website',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_global.scss" as *;`,
+      },
+    },
+  },
+})
